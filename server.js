ebf2891953513b3b1e4b3609c180491867d409bc
agian <angelos.giannourakos@gmail.com>
2022-06-03 08:14:58 +0300

    Some changes

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(3000, () => {
+app.post('/quotes', (req, res)=>{
+    console.log('this is a post')
+})
+
+const PORT = 3000
+
+app.listen(process.env.PORT || PORT, () => {
   console.log('listening on port 3000');
 });
 
