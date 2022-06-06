6b2b273c931ebe4bbe93116617994bdfea253324
agian <angelos.giannourakos@gmail.com>
2022-06-06 08:47:24 +0300

    Adding collections

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 // const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 3000 ;
 
 const connect = 'mongodb+srv://angelosgian:kyX8mjtH516j29Pa@cluster0.hxcp0.mongodb.net/?retryWrites=true&w=majority'
 
@@ -14,24 +14,27 @@ const connect = 'mongodb+srv://angelosgian:kyX8mjtH516j29Pa@cluster0.hxcp0.mongo
 MongoClient.connect(connect)
     .then(client => {
         console.log('Connected to mongoDB');
-    })
-    .catch(err => console.log(err))
+        const db = client.db('star-wars-quotes');
+        const collection = db.collection('quotes');
     
-// app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.urlencoded({extended: true}))
+        // app.use(bodyParser.urlencoded({extended: true}))
+        app.use(express.urlencoded({extended: true}))
 
 
-app.get('/', (req, res)=> {
-    res.sendFile(__dirname + '/public/index.html')
-})
+        app.get('/', (req, res)=> {
+            res.sendFile(__dirname + '/public/index.html')
+        })
 
-app.post('/quotes', (req, res)=>{
-    console.log(req.body)
-})
+        app.post('/quotes', (req, res)=>{
+            console.log(req.body)
+        })
 
-const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`It's alive on port ${PORT}`);
-});
 
+        app.listen(PORT, () => {
+          console.log(`It's alive on port ${PORT}`);
+        });  
+    
+    
+    })
+    .catch(err => console.log(err));
