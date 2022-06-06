93a3b8c291aed7a18bf81fa31ead77bc0e1826f9
agian <angelos.giannourakos@gmail.com>
2022-06-06 08:33:10 +0300

    Adding mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+// const bodyParser = require('body-parser');
 
 
+const connect = 'mongodb+srv://angelosgian:kyX8mjtH516j29Pa@cluster0.hxcp0.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connect, (err, client) => {
+  if(err) return console.log(err);
+  console.log('Connected to mongoDB');
+
+})
 // app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/public/index.html')
@@ -14,9 +23,9 @@ app.post('/quotes', (req, res)=>{
     console.log(req.body)
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(process.env.PORT || PORT, () => {
-  console.log('listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`It's alive on port ${PORT}`);
 });
 
