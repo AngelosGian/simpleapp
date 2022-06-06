212796acf9e006c10b644c1effd575916a9442c8
agian <angelos.giannourakos@gmail.com>
2022-06-06 08:36:56 +0300

    Changing db connection to promise

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const MongoClient = require('mongodb').MongoClient;
 
 const connect = 'mongodb+srv://angelosgian:kyX8mjtH516j29Pa@cluster0.hxcp0.mongodb.net/?retryWrites=true&w=majority'
 
-MongoClient.connect(connect, (err, client) => {
-  if(err) return console.log(err);
-  console.log('Connected to mongoDB');
+// MongoClient.connect(connect, (err, client) => {
+//   if(err) return console.log(err);
+//   console.log('Connected to mongoDB');
 
-})
+// })
+MongoClient.connect(connect)
+    .then(client => {
+        console.log('Connected to mongoDB');
+    })
+    .catch(err => console.log(err))
+    
 // app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true}))
 
