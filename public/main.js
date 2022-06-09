fd7a09c7092f2538df27018f1e2abe44dd817444
agian <angelos.giannourakos@gmail.com>
2022-06-09 23:45:31 +0300

    minor changes

--- public/main.js
+++ public/main.js
@@ -2,10 +2,11 @@ const updateButton = document.querySelector('#update-button');
 
 updateButton.addEventListener('click', _ => {
     fetch('/quotes',{
-        method: 'put'
+        method: 'put',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             name: 'Darth Vader',
             quote: 'I find your lack of faith disturbing.'
+        })
     })
 })
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 // const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000 ;
+
+// app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const connect = 'mongodb+srv://angelosgian:kyX8mjtH516j29Pa@cluster0.hxcp0.mongodb.net/?retryWrites=true&w=majority'
 
@@ -18,6 +20,7 @@ MongoClient.connect(connect)
         const collections = db.collection('quotes');
         app.use(express.static('public'))
         // app.use(bodyParser.urlencoded({extended: true}))
+        app.use(express.json())
         app.use(express.urlencoded({extended: true}))
 
 
@@ -41,8 +44,14 @@ MongoClient.connect(connect)
             })
         app.put('/quotes', (req, res) => {
           console.log(req.body)
+          collections.findOneAndUpdate(
+            {name: 'Yoda'},
+            update,
+            options
+            )
+            .then(result => {})
+            .catch(error => console.error(error))
         })
-
         app.listen(PORT, () => {
           console.log(`It's alive on port ${PORT}`);
         });  
